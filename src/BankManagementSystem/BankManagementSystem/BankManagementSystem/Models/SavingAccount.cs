123de6c7b9c3cfd556c2f17fe08d8ee3b873ea35
aliabooof <aliabooof42@gmail.com>
2024-12-30 15:01:17 +0200

    Creating Saving account

---
+++ src/BankManagementSystem/BankManagementSystem/BankManagementSystem/Models/SavingAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagementSystem.Models
+{
+    class SavingAccount :Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingAccount(string accountNumber, double initialBalance, double _interestRate)
+            : base(accountNumber , initialBalance)
+            => InterestRate = _interestRate;
+
+        public void ApplyInterest()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Interest Rate: {InterestRate * 100}%";
+        }
+
+
+
+    }
+}
