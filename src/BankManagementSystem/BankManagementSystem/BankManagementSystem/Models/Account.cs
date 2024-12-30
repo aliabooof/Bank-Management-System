78b13379c19418b5f9b8816e221625912e397cc2
aliabooof <aliabooof42@gmail.com>
2024-12-30 15:00:21 +0200

    creating abstract base class for accounts

---
+++ src/BankManagementSystem/BankManagementSystem/BankManagementSystem/Models/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagementSystem.Models
+{
+    public abstract class Account
+    {
+        public string AccountNumber { get; set; }
+
+        public double Balance { get; set; }
+
+        protected Account(string _AccountNumber, double initialBalance)
+        {
+            AccountNumber = _AccountNumber;
+            Balance = initialBalance;
+        }
+
+        public virtual void Deposit(double Amount)
+          => Balance += Amount;
+
+        public virtual bool Withdraw(double Amount)
+        {
+            if (Amount > Balance)
+                return false;
+            Balance -= Amount;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"Account Number: {AccountNumber}, Balance: {Balance}";
+        }
+
+    }
+}
+
