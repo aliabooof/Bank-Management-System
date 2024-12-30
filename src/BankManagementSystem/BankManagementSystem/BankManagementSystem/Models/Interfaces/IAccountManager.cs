a88b4b244e771f92a501d8d3c9ccb7967bfe71d2
aliabooof <aliabooof42@gmail.com>
2024-12-30 14:54:11 +0200

    create an interface to manage the accounts

---
+++ src/BankManagementSystem/BankManagementSystem/BankManagementSystem/Models/Interfaces/IAccountManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagementSystem.Models.Interfaces
+{
+    interface IAccountManager
+    {
+        void CreateAccount(string accountNumber, double initialBalance);
+        void ListAccounts();
+    }
+}
