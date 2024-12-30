2216bf6e79a31fa7d483241f7ca63407d224dc02
aliabooof <aliabooof42@gmail.com>
2024-12-30 14:59:38 +0200

    Creating Interface for processing transactions

---
+++ src/BankManagementSystem/BankManagementSystem/BankManagementSystem/Interfaces/ITransactionProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagementSystem.Interfaces
+{
+    interface ITransactionProcessor
+    {
+        void Deposit(string accountNumber, double Amount);
+        void Withdraw(string accountNumber, double Amount);
+    }
+}
